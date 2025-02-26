d517a323cb100f10176b2619dae0c1e686747224
Nihal Singh <142614932+Emperorns@users.noreply.github.com>
2025-02-26 23:42:30 +0530

    Delete koyeb-bot/scr/db/models/User.js

--- koyeb-bot/scr/db/models/User.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const UserSchema = new mongoose.Schema({
-  telegramId: { type: Number, required: true, unique: true },
-  createdAt: { type: Date, default: Date.now }
-});
-
-export default mongoose.model('User', UserSchema);
