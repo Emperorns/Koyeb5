3d52820020c1424463c0a028193aefa5e4146c87
Nihal Singh <142614932+Emperorns@users.noreply.github.com>
2025-02-26 23:27:12 +0530

    Update encryption.js

--- scr/utils/encryption.js
+++ scr/utils/encryption.js
@@ -1,1 +1,10 @@
+import CryptoJS from 'crypto-js';
 
+export const encrypt = (text) => {
+  return CryptoJS.AES.encrypt(text, process.env.ENCRYPTION_KEY).toString();
+};
+
+export const decrypt = (ciphertext) => {
+  const bytes = CryptoJS.AES.decrypt(ciphertext, process.env.ENCRYPTION_KEY);
+  return bytes.toString(CryptoJS.enc.Utf8);
+};
